e4bfdb0b986999624540c791850988b1df2b93cd
Jess McDonald <51135336+jessmcdonald@users.noreply.github.com>
2020-02-12 12:09:01 +0100

    add login link

--- client/src/App.js
+++ client/src/App.js
@@ -6,17 +6,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Login to Spotify
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          className="Login-link"
+          href="https://spotify-spot.herokuapp.com/login"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Click to login
         </a>
       </header>
     </div>
