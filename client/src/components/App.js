10a9fd7b3285afff76fe519107d61dc175e106ad
Jess McDonald <51135336+jessmcdonald@users.noreply.github.com>
2020-02-13 11:05:08 +0100

    add Circular font

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import axios from "axios";
-import { getHashParams } from '../spotify';
+import { getHashParams } from "../spotify";
 
 const params = getHashParams();
 const access_token = params.access_token;
 const refresh_token = params.access_token;
 const error = params.error;
 const token = access_token;
-const headers = { Authorization: `Bearer ${token}`, 'Content-Type': 'application/json', }
+const headers = {
+  Authorization: `Bearer ${token}`,
+  "Content-Type": "application/json"
+};
 
 class App extends React.Component {
   constructor() {
@@ -19,62 +22,60 @@ class App extends React.Component {
     this.state = {
       user: null
     };
-}
+  }
 
-componentDidMount(){
-  this.getUserInfo()
-}
+  componentDidMount() {
+    this.getUserInfo();
+  }
 
-//get user
-getUserInfo = () => {
-    axios.get("https://api.spotify.com/v1/me",
-      {
+  //get user
+  getUserInfo = () => {
+    axios
+      .get("https://api.spotify.com/v1/me", {
         headers: { Authorization: `Bearer ${access_token}` }
       })
       .then(response => {
-        this.setState({ user: response.data});
-        console.log(this.state.user)
+        this.setState({ user: response.data });
+        console.log(this.state.user);
       })
-      .catch(function (error) {
+      .catch(function(error) {
         console.log(error);
       });
-};
+  };
 
-render() {
-
-  return (
-  
-    <div className="App">
-      <header className="App-header">
-      </header>
-        
-
-  <body>
-    <div class="container">
-      
-      {this.state.user ? (
-        <div id="loggedin">
-        <div id="user-profile">
-          Logged in as {this.state.user.display_name}
-        </div>
-        <div id="oauth">
-        </div>
-        <button class="btn btn-default" id="obtain-new-token" >Obtain new token using the refresh token</button>
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header"></header>
+        <body>
+          <div class="container">
+            {this.state.user ? (
+              <div id="loggedin">
+                <div id="user-profile">
+                  Logged in as {this.state.user.display_name}
+                </div>
+                <div id="oauth"></div>
+                <button class="btn btn-default" id="obtain-new-token">
+                  Obtain new token using the refresh token
+                </button>
+              </div>
+            ) : (
+              <div id="login" className="App-login">
+                <h1>Spotify Spot</h1>
+                <a
+                  href="/login"
+                  class="btn btn-primary"
+                  onClick={() => this.getUserInfo()}
+                >
+                  Log in with Spotify
+                </a>
+              </div>
+            )}
+          </div>
+        </body>
       </div>
-
-      ) : (
-        <div id="login" className="App-login">
-        <h1>Spotify Spot</h1>
-        <a href="/login" class="btn btn-primary" onClick={() => this.getUserInfo()}>Log in with Spotify</a>
-      </div>
-      )
-      }
-      
-    </div>
-  </body>
-    </div>
-  );
-}
+    );
+  }
 }
 
 export default App;
